6d027c23bdb30aa5bb5074959f1cdbe16b80f6b4
Niwatori401 <->
2024-07-14 15:21:56 -0700

    No longer apply sleeping alone thoughts for pawns sleeping on blob bed

---
+++ Source/RimRound/RimRound/Things/Building_BlobBed.cs
@@ -0,0 +1,21 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace RimRound.Things
+{
+    public class Building_BlobBed : Building_Bed
+    {
+        public Pawn originatingPawn;
+
+        public override void ExposeData()
+        {
+            base.ExposeData();
+            Scribe_References.Look<Pawn>(ref originatingPawn, "originatingPawn");
+        }
+    }
+}
