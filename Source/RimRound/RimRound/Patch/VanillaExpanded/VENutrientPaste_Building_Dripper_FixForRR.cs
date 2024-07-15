ba44f751d550b9e71d30c0b9cd31aac2552ed49b
Niwatori401 <->
2024-07-14 17:44:46 -0700

    Fix bug with VE Nutrient Paste Dripper

---
+++ Source/RimRound/RimRound/Patch/VanillaExpanded/VENutrientPaste_Building_Dripper_FixForRR.cs
@@ -0,0 +1,93 @@
+﻿using HarmonyLib;
+using RimRound.Utilities;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using RimRound.Comps;
+
+namespace RimRound.Patch
+{
+    public static class VENutrientPaste_Building_Dripper_FixForRR
+    {
+        private static MethodInfo methodInfo_GetCurLevelPercentage = typeof(Need).GetProperty(nameof(Need.CurLevelPercentage)).GetGetMethod();
+        private static MethodInfo methodInfo_ShouldntEat = typeof(VENutrientPaste_Building_Dripper_FixForRR).GetMethod(nameof(ShouldntEat), BindingFlags.NonPublic | BindingFlags.Static);
+
+        private static bool ShouldntEat(Pawn pawn)
+        {
+            bool shouldEat = false;
+            if (pawn.TryGetComp<FullnessAndDietStats_ThingComp>() is FullnessAndDietStats_ThingComp fullnessComp && pawn.needs?.food != null)
+            {
+                switch (fullnessComp.DietMode)
+                {
+                    case DietMode.Nutrition:
+                        shouldEat = pawn.needs.food.CurLevel + fullnessComp.CurrentFullness / fullnessComp.CurrentFullnessToNutritionRatio <= fullnessComp.GetRanges().First;
+                        break;
+                    case DietMode.Hybrid:
+                        shouldEat = pawn.needs.food.CurLevel + fullnessComp.CurrentFullness / fullnessComp.CurrentFullnessToNutritionRatio <= fullnessComp.GetRanges().First;
+                        break;
+                    case DietMode.Fullness:
+                        shouldEat = fullnessComp.CurrentFullness <= fullnessComp.GetRanges().First;
+                        break;
+                    case DietMode.Disabled:
+                        // How VE has it hard-coded
+                        shouldEat = pawn.needs.food.CurLevelPercentage <= 0.4;
+                        break;
+                    default:
+                        return false;
+                }
+            }
+
+            return !shouldEat;
+        }
+
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+        {
+            List<CodeInstruction> codeInstructions = new List<CodeInstruction>(instructions);
+            List<CodeInstruction> newInstructions = new List<CodeInstruction>();
+
+            if (methodInfo_GetCurLevelPercentage == null) {
+                Log.Error("Failed to patch VENutrientPaste_Building_Dripper_FixForRR - Null MethodInfo");
+                return codeInstructions;
+            }
+
+
+            int startJndex = -1;
+            int endJndex = -1;
+
+            for (int i = 0; i < codeInstructions.Count; ++i) {
+                if (codeInstructions[i].opcode == OpCodes.Callvirt && (MethodInfo)codeInstructions[i].operand == methodInfo_GetCurLevelPercentage) {
+                    Log.Message("Found Insertion Point");
+                    startJndex = i - 2; // Just after pawn is loaded on stack
+                    endJndex = i + 3; // Just before 0 is loaded on stack
+                    break;
+                }
+            }
+
+            newInstructions.Add(new CodeInstruction(OpCodes.Call, methodInfo_ShouldntEat));
+
+            if (startJndex != -1 && endJndex != -1)
+            {
+                codeInstructions.RemoveRange(startJndex, endJndex - startJndex + 1);
+                codeInstructions.InsertRange(startJndex, newInstructions);
+            }
+
+            return codeInstructions;
+        }
+
+
+        public static PatchCollection GetPatchCollection()
+        {
+            return new PatchCollection
+            {
+                transpiler = typeof(VENutrientPaste_Building_Dripper_FixForRR).GetMethod(
+                    nameof(VENutrientPaste_Building_Dripper_FixForRR.Transpiler), ModCompatibilityUtility.majorFlags)
+            };
+        }
+    }
+}
