f43814f2dcf3a3f0b498077355c01b90361783b0
Seirra <sophietheopossum@yandex.ru>
2025-10-15 20:10:08 +0100

    Clean up unneeded using statements
    Removes various System namespace imports and `RimRound.UI` as they are no longer required by the file, improving code cleanliness.

--- Source/RimRound/RimRound/Patch/RimWorldPatches/DietAIPatches/Need-Food_NutritionWanted_AdjustAIForDietMode.cs
+++ Source/RimRound/RimRound/Patch/RimWorldPatches/DietAIPatches/Need-Food_NutritionWanted_AdjustAIForDietMode.cs
@@ -1,13 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using RimWorld;
+﻿using RimWorld;
 using Verse;
 using HarmonyLib;
-using RimRound.UI;
 using RimRound.Comps;
 using RimRound.Utilities;
 
